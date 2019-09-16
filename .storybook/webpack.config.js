eaac317d1195a2771ec01259626a00a492cebedb
Jerric Lyns John <jerriclynsjohn@outlook.com>
2019-09-16 14:48:44 +0530

    Adding eslint and prettier

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,29 +1,30 @@
 const path = require('path');
 
 module.exports = ({ config, mode }) => {
+    config.module.rules.push(
+        {
+            test: /\.css$/,
+            loaders: [
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: true,
+                        config: {
+                            path: './.storybook/',
+                        },
+                    },
+                },
+            ],
 
-  config.module.rules.push({
-    test: /\.css$/,
-    loaders: [
-      {
-        loader: 'postcss-loader',
-        options: {
-          sourceMap: true,
-          config: {
-            path: './.storybook/',
-          },
+            include: path.resolve(__dirname, '../storybook/'),
         },
-      },
-    ],
+        {
+            test: /\.stories\.js?$/,
+            loaders: [require.resolve('@storybook/addon-storysource/loader')],
+            include: [path.resolve(__dirname, '../storybook')],
+            enforce: 'pre',
+        },
+    );
 
-    include: path.resolve(__dirname, '../storybook/'),
-  },
-  {
-    test: /\.stories\.js?$/,
-    loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    include: [path.resolve(__dirname, '../storybook')],
-    enforce: 'pre',
-  });
-
-  return config;
+    return config;
 };
